71e6f5c3f735ad9fda30758572c9083e8c6b5eeb
Александр Иванченко <xKapellMeisterx@yandex.ru>
2022-10-24 18:31:03 +0300

    'add TagsViewSet and migrations'

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from recipes.models import Tag
 
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = (
+            'id',
+            'name',
+            'hexcolor',
+            'slug'
+        )
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,1 +1,9 @@
+from rest_framework import viewsets
 
+from recipes.models import Tag
+from .serializers import TagSerializer
+
+
+class TagsViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
